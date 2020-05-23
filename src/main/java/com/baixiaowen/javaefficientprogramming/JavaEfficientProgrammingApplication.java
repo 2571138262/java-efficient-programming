e0ac142d26a213468bf3fab36ec839b1cb7aa515
2571138262 <2571138262@qq.com>
2020-05-23 11:36:58 +0800

    first commit

---
+++ src/main/java/com/baixiaowen/javaefficientprogramming/JavaEfficientProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.baixiaowen.javaefficientprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEfficientProgrammingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaEfficientProgrammingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/baixiaowen/javaefficientprogramming/JavaEfficientProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baixiaowen.javaefficientprogramming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEfficientProgrammingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
