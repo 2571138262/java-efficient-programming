1693acb1773d11d5d2ecff2436ae5035550a8aed
2571138262 <2571138262@qq.com>
2020-06-08 22:31:28 +0800

    Stream 流编程 实战案例 - 20200608 晚上 在家

--- Stream流编程.md
+++ Stream流编程.md
@@ -119,3 +119,80 @@
                          BiFunction<U, ? super T, U> accumulator,
                          BinaryOperator<U> combiner);
 ```
+
+#### (2)、汇总
+##### 将Stream流中元素转换成一个容器！！！
+```java
+    import java.util.List;import java.util.Map;import java.util.stream.Collectors;import java.util.stream.Stream;class A {
+        void method(){
+            Stream<Integer> integerStream = Lists.newArrayList(1, 2, 3, 4).stream();
+            
+            // 转换成List集合
+            List<Integer> list = integerStream.collect(Collectors.toList());
+            // 按奇偶区分
+            Map<Boolean, List<Integer>> partitions = integerStream.collect(
+                Collectors.partitioningBy(item -> item % 2 == 0));
+            // 按元素分组
+            Map<Integer, List<Integer>> groups = integerStream.collect(Collectors.groupingBy(item -> item));
+        }
+    }   
+```
+##### collect 接口参数
+
+```java
+    class A{
+        /**
+        * 汇总操作接口定义
+        * @param supplier - 初始化结果容器
+        * @param accumulator - 添加元素到结果容器逻辑
+        * @param combiner - 并行执行时多个结果容器的合并方式
+        * @param <R> - 元素类型
+        * @return 
+        */
+        <R> R collect(Supplier<R> supplier,
+                          BiConsumer<R, ? super T> accumulator,
+                          BiConsumer<R, R> combiner);
+    }   
+```
+
+#### 总结：自定义的汇总和归约方法，在定义并行结果合并的时候，需要处理的点有很多，因为并行的时候就会涉及到线程，涉及到冲突，涉及到原子性等等，所以非常不建议自己去做这种自定义的逻辑实现
+###### JDK为我们封装了一些非常常用的归约和汇总操作，覆盖了我们使用到的大量的场景，所以推荐使用这些
+
+```java
+    class A {
+        /**
+        * 汇总操作接口定义
+        * @param collector - Collector接口实现类 收集器接口
+        * @param <R> 
+        * @param <A>
+        * @return 
+        */
+        <R, A> R collect(Collector<? super T, A, R> collector);
+    }
+```
+
+```java
+    /**
+    * Collector 接口
+    * @param <T> 流中要收集的项目的泛型
+    * @param <A> 累加器的类型，累加器是在收集过程中用于累积部分结果的对象
+    * @param <R> 收集操作得到的对象的类型
+    * 例如 ：public class ToList<T> implements Collector<T, List<T>, List<T>>
+    */
+    public interface Collector<T, A, R> {
+        // 建立新的结果容器
+        Supplier<A> supplier();
+        // 将元素添加到结果容器
+        BiConsumer<A, T> accumulator();
+        // 合并连个结果容器
+        BinaryOperator<A> combiner();
+        // 对结果容器应用最终转换 
+        Function<A, R> finisher();
+        // 定义收集器行为，如 ：是否可以并行，可以使用那些优化
+        Set<Characteristics> characteristics();
+    }
+```
+
+### 9、实际案例
+
+#### (1)、班级中有20名学生，每名学生有5门课的考试成绩，其中缺考的科目分数字段为空。需要找出有缺考的学生都叫什么名字。
---
+++ src/test/java/com/baixiaowen/javaefficientprogramming/stream/cases/CaseOne.java
@@ -0,0 +1,112 @@
+package com.baixiaowen.javaefficientprogramming.stream.cases;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 案例一：班级中有20名学生，每名学生有5门课的考试成绩，其中缺考的科目分数字段为空。需要找出有缺考的学生都叫什么名字。
+ */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class CaseOne {
+
+    /**
+     * 考试成绩模型
+     */
+    @Data
+    @AllArgsConstructor
+    class ExamStudentScore{
+        // 学生姓名
+        private String studentName;
+        // 成绩
+        private Integer scoreValue;
+        // 科目名称
+        private String subject;
+    }
+
+    /**
+     * 学生考试成绩
+     */
+    Map<String, List<ExamStudentScore>> studentMap;
+
+    @BeforeAll
+    public void init() {
+        studentMap = new HashMap<>();
+
+        List<ExamStudentScore> zsScoreList = new ArrayList<>();
+        zsScoreList.add(
+                new ExamStudentScore(
+                        "张三",
+                        30,
+                        "CHINESE"));
+        zsScoreList.add(
+                new ExamStudentScore(
+                        "张三",
+                        40,
+                        "ENGLISH"));
+        zsScoreList.add(
+                new ExamStudentScore(
+                        "张三",
+                        50,
+                        "MATHS"));
+        studentMap.put("张三", zsScoreList);
+
+        List<ExamStudentScore> lsScoreList = new ArrayList<>();
+        lsScoreList.add(
+                new ExamStudentScore(
+                        "李四",
+                        80,
+                        "CHINESE"));
+        lsScoreList.add(
+                new ExamStudentScore(
+                        "李四",
+                        null,
+                        "ENGLISH"));
+        lsScoreList.add(
+                new ExamStudentScore(
+                        "李四",
+                        100,
+                        "MATHS"));
+        studentMap.put("李四", lsScoreList);
+
+        List<ExamStudentScore> wwScoreList = new ArrayList<>();
+        wwScoreList.add(
+                new ExamStudentScore(
+                        "王五",
+                        null,
+                        "CHINESE"));
+        wwScoreList.add(
+                new ExamStudentScore(
+                        "王五",
+                        null,
+                        "ENGLISH"));
+        wwScoreList.add(
+                new ExamStudentScore(
+                        "王五",
+                        70,
+                        "MATHS"));
+        studentMap.put("王五", wwScoreList);
+    }
+
+    @Test
+    public void findStudent() {
+        studentMap.forEach((studentName, scoreList) -> {
+            boolean bool = scoreList.stream().anyMatch(score -> {
+                // TODO anyMatch 找到任意一条符合条件的数据后就停止
+                System.err.println(score);
+                return score.getScoreValue() == null;
+            });
+            if (bool) {
+                System.err.println("此学生【" + studentName + "】有缺考情况");
+            }
+        });
+    }
+
+}
