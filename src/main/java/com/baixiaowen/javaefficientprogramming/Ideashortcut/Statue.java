0437d5100dc4c4b615369f96bac9414eb7db6f41
2571138262 <2571138262@qq.com>
2020-06-18 22:28:57 +0800

    IDEA开发工具学习 - 20200617 晚上 在家

---
+++ src/main/java/com/baixiaowen/javaefficientprogramming/Ideashortcut/Statue.java
@@ -0,0 +1,51 @@
+package com.baixiaowen.javaefficientprogramming.Ideashortcut;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * IDEA 快捷键
+ */
+public class Statue {
+//     continue(100),
+//     processing(102),
+//     checkpoint(103),
+//     found(302),
+//     done(410),
+
+     /**
+      * psc
+      */
+     private String end ;
+
+     /**
+      * 测试
+      */
+     private String name;
+
+     /**
+      * pic
+      */
+     private Integer aaa;
+
+
+
+
+     public static void main(String[] args) {
+          ArrayList<Object> arrayList = new ArrayList<>();
+
+          List<Integer> list = Arrays.asList(1,2,3,4,5);
+
+          System.err.println();
+
+          list.forEach(item -> System.err.println(item));
+
+          if (arrayList != null) {
+
+          }
+
+
+     }
+
+}
