3e47dde85586ca2e4c444bd0c56fb96d5b6c7d50
2571138262 <2571138262@qq.com>
2020-06-21 21:22:01 +0800

    finally - 20200621 晚上 在家

--- src/main/java/com/baixiaowen/javaefficientprogramming/stream/MapVs.java
+++ src/main/java/com/baixiaowen/javaefficientprogramming/stream/MapVs.java
@@ -3,7 +3,6 @@ package com.baixiaowen.javaefficientprogramming.stream;
 import com.baixiaowen.javaefficientprogramming.lambda.cart.Sku;
 
 import java.util.Optional;
-import java.util.function.Function;
 
 public class MapVs {
     public static String apply(Sku sku) {
